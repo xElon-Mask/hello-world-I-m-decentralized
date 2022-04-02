000101ec572e8d4e98d9b04461cd40cc127b5852
xElon-Mask <xelonmask@protonmail.com>
2022-04-02 19:32:36 +0200

    write smart contract with unique function hi

---
+++ HelloWord.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.13;
+
+contract HelloWorld {
+    function hi() pure public returns(string memory) {
+            return "Bonsoir Elliot";
+    }
+}
